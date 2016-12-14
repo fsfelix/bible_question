8de045946945e96ab5fdd28cf7208d916dce5e64
Felipe Felix <felipescript@gmail.com>
2016-12-14 21:40:52 +0000

    Acts as votable: question

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :questions do
+    member do
+      get "like", to: "questions#upvote"
+      get "dislike", to: "questions#downvote"
+    end
     resources :answers
   end
   root 'questions#index'
