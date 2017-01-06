6adb8810a2744c24bed460e938f3d515bca49b1d
Felipe Felix <felipescript@gmail.com>
2017-01-06 12:48:39 -0200

    Added pt-br translation.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,10 @@ module BibleQuestion
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # For some strange reason, I couldn't add :pt-BR, so I changed the pt-BR.yml
+    # for pt.yml, and now it works.
+    
     config.i18n.default_locale = :pt
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
